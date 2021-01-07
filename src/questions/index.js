3da78b6aca90184a691f10420150c7bd37ced2d8
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-07 15:05:31 +0000

    can now post questions with vaildation

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const uniqid = require("uniqid");
+const { check, validationResult } = require("express-validator");
 const { readQuestions, writeQuestions } = require("../lib/utilities");
 
 const Router = express.Router();
@@ -29,5 +30,48 @@ Router.delete("/:questionID", async (req, res) => {
     res.send(error);
   }
 });
+
+Router.post(
+  "/",
+  [
+    check("duration")
+      .exists()
+      .isInt()
+      .withMessage("Give a duration for the question"),
+    check("text")
+      .exists()
+      .isLength({ min: 3 })
+      .withMessage("Give the actual question"),
+    check("answers")
+      .exists()
+      .isArray({ min: 4, max: 4 })
+      .withMessage("Provide at least four answers"),
+    check("answers.*.isCorrect")
+      .exists()
+      .isBoolean()
+      .withMessage("You must show if answers are true or false with a boolean"),
+    check("answers.*.text").exists().withMessage("Give text of answer"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.send(errors);
+    } else {
+      try {
+        const questionDB = await readQuestions();
+        questionDB.push({
+          ...req.body,
+          _id: uniqid(),
+        });
+        await writeQuestions(questionDB);
+        res.send("Added Question!");
+      } catch (error) {
+        console.log(error);
+        res.send(error);
+      }
+    }
+  }
+);
 
 module.exports = Router;
